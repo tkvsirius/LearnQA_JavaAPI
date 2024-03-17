bd2439d32fd1f23a6444eca328957981a374ee2c
Sirius <tkvsirius@gmail.com>
2024-03-17 18:51:49 +0300

    'add 2'

--- src/test/java/helloWorldTest.java
+++ src/test/java/helloWorldTest.java
@@ -1,10 +1,31 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class helloWorldTest {
 
     @Test
-    public void testHelloWorld(){
-        System.out.println("Hello World !");
+    public void testRestAssured(){
+        /*Map<String, Object> body = new HashMap<>();
+        body.put("param1", "value1");
+        body.put("param2", "value2");*/
+
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(true)
+                .when()
+                .get("https://playground.learnqa.ru/api/get_303")
+                .andReturn();
+
+        int statuscode = response.statusCode();
+        System.out.println(statuscode);
+
     }
 
 }
